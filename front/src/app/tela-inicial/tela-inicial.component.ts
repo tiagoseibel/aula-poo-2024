9540e50161cf1544d4adf15ac9eb3c852b0c90eb
Tiago Seibel <tiagoseibel@gmail.com>
2024-09-05 01:46:10 +0000

    exemplo da aula

--- front/src/app/tela-inicial/tela-inicial.component.ts
+++ front/src/app/tela-inicial/tela-inicial.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Transacao } from '../../model/transacao';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tela-inicial',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tela-inicial.component.html',
   styleUrl: './tela-inicial.component.css'
 })
-export class TelaInicialComponent {
+export class TelaInicialComponent implements OnInit{
+  extrato: Transacao[] = [];
+  texto: string = "EXTRATO DO CLIENTE"
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+      let tran: Transacao = {
+        data: new Date(),
+        descricao: "PIX 0001",
+        valor: 10.00,
+        operacao: "C",
+        numero: 1
+      };
+      this.extrato.push(tran);
+      
+  }
 
 }
---
+++ front/src/model/transacao.ts
@@ -0,0 +1,8 @@
+
+export interface Transacao {
+    numero: number;
+    descricao: string;
+    valor: number;
+    data: Date;
+    operacao: string;
+}
