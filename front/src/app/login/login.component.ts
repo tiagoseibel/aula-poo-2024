9d10923f1ed6aeed7e1d32a1bbef8c79cfbbbd81
Tiago Seibel <tiagoseibel@gmail.com>
2024-12-04 23:53:33 +0000

    front-end simples login

--- front/src/app/login/login.component.ts
+++ front/src/app/login/login.component.ts
@@ -1,22 +1,37 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { InputText, InputTextModule } from 'primeng/inputtext';
+import { Usuario } from '../../model/usuario';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [InputTextModule, ButtonModule],
+  imports: [InputTextModule, ButtonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
-  constructor(private router: Router) {
+  constructor(
+    private router: Router,
+    private httpClient: HttpClient
+  ) {
 
   }
 
+  usuario = new Usuario();
+
   public fazerLogin() {
-    this.router.navigate(['/tela-inicial']);
+    this.httpClient.post("http://localhost:8081/usuario/login", this.usuario)
+    .subscribe(retorno => {
+       if(retorno == true) {
+         this.router.navigate(['/tela-inicial']);
+       } else {
+        alert("Usuario ou senha incorretos");
+       }
+    });
   }
 }
